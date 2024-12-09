4ea58910b8559175a9dee39a3b4f4080f91b4aac
Luu Thuong Hong <luuthuonghong@gmail.com>
2024-12-09 20:11:31 +0700

    http listener

---
+++ client/src/http_listener.h
@@ -0,0 +1,18 @@
+// http_listener.h
+#pragma once
+#include <string>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+class HttpListener {
+public:
+    HttpListener(int port = 8080);
+    ~HttpListener();
+    std::string waitForAuthorizationCode();
+
+private:
+    SOCKET serverSocket;
+    bool initializeWinsock();
+    std::string parseAuthorizationCode(const std::string& request);
+};
